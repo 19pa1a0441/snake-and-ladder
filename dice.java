21634001544e4c735774ad7d3cd6765e3c757e99
19pa1a0441 <60639481+19pa1a0441@users.noreply.github.com>
2021-12-15 21:55:32 +0530

    Update dice.java

--- dice.java
+++ dice.java
@@ -2,9 +2,7 @@ public class Dice {
     private int numberOfDices;
     Dice(int numberOfDices) {
         this.numberOfDices = numberOfDices;
-    }
-    
-    
+    }  
     int rollDice(){
         int dice = ((int) (Math.random()*(6*numberOfDices - 1*numberOfDices))) + 1;
         return dice;
